9c864a0fadd577598bdb8f8ccd23751e185ffc40
Eduardo007-lang <eduardo2018ads@hotmail.com>
2024-01-01 18:12:14 -0300

    feat: added request method

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("hello")
+
+const limit = 0;
+const  offset = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+// Request Method - Metodo Http 
+// Post, get, put, delete e update.
+//Query String - Parte da url que e usado para enviar dados adicionais, paramentros de dados que queremos 
+
+
+fetch(url).then((res) => {
+    console.log(res);
+})
